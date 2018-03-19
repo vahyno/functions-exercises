98a30e9df47e7cd86590ae73c3e08249fb1db2a8
vahyno <vahyno@gmail.com>
2018-03-19 14:16:41 -0700

    part of the solution

--- functions.js
+++ functions.js
@@ -1,1 +1,45 @@
 // TODO: add your solutions here!
+
+function power(num, exp) {
+  let powered = 1;
+    for (var i = 1; i <= exp; i++) {
+      powered*=num;
+    }
+    return powered;
+}
+
+function areaOfCircle(radius) {
+    return 3.14*radius*radius;
+}
+
+function pythagoreanTheorem(a,b){
+  let c = power(a,2)+power(b,2)
+  return c;
+}
+
+function printTriangle(num){
+  let star = "";
+  for (var i = 0; i < num; i++) {
+    star += "*"
+    console.log(star);
+  }
+}
+
+// 12. Stretch Challenge: Can you alter the printTriangle function to create a Pyramid?
+// Example: printPyramid(10);
+//
+//            *
+//           * *
+//          * * *
+//         * * * *
+//        * * * * *
+//       * * * * * *
+//      * * * * * * *
+//     * * * * * * * *
+//    * * * * * * * * *
+//   * * * * * * * * * *
+// Warning: This is a surprisingly tricky interview-level exercise. Try at your own risk!
+
+function printPyramid(length) {
+	// TODO: Place your code here
+}
